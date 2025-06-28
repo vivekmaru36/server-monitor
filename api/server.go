345d3deba822c5e2fb7b0ab472e4938600d9b789
vivekmaru36 <vivekmaru036@gmail.com>
2025-06-28 13:56:59 +0530

    Api creation

---
+++ api/server.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"server-monitor/monitor"
+)
+
+type Stats struct(
+	Uptime string `json:"uptime"`
+	CPUUsage float64 `json:"cpu_usage`
+	MemoryUsage float64 `json:"memory_usage"`
+	DiskUsage float64 `json:"disk_usage"`
+) 
+
+func statsHandler(w http.ResponseWriter,r *http.Request)  {
+	cpu, _ := monitor.GetCPUUsage()
+	mem, _ := monitor.GetMemoryUsage()
+	disk, _ := monitor.GetDiskUsage()
+	uptime, _ := monitor.GetUptime()
+	
+	stats := Stats{
+		Uptime: uptime,
+		CPUUsage: cpu,
+		MemoryUsage:mem,
+		DiskUsage:disk,
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(stats)
+}
+
+func uptimeHandler(w http.ResponseWriter, r *http.Request)  {
+	uptime,err:=monitor.GetUptime()
+	if err != nil {
+		http.Error(w,"Failed to get uptime",http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(uptime))
+}
+
+// imp stuff
+func StartServer()  {
+	http.HandleFunc("/stats",statsHandler)
+	http.HandleFunc("/uptime",uptimeHandler)
+
+	http.ListenAndServe(":8080",nil)
+}
--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"server-monitor/monitor"
+	"api"
 )
 
 func main() {
-	cpu, _ := monitor.GetCPUUsage()
-	mem, _ := monitor.GetMemoryUsage()
-	disk, _ := monitor.GetDiskUsage()
-	uptime, _ := monitor.GetUptime()
+	// cpu, _ := monitor.GetCPUUsage()
+	// mem, _ := monitor.GetMemoryUsage()
+	// disk, _ := monitor.GetDiskUsage()
+	// uptime, _ := monitor.GetUptime()
 
-	fmt.Printf("Uptime: %s\n", uptime)
-	fmt.Printf("CPU Usage: %.2f%%\n", cpu)
-	fmt.Printf("Memory Usage: %.2f%%\n", mem)
-	fmt.Printf("Disk Usage: %.2f%%\n", disk)
+	// fmt.Printf("Uptime: %s\n", uptime)
+	// fmt.Printf("CPU Usage: %.2f%%\n", cpu)
+	// fmt.Printf("Memory Usage: %.2f%%\n", mem)
+	// fmt.Printf("Disk Usage: %.2f%%\n", disk)
+
+	fmt.Println("Server running on http://localhost:8080")
+	api.StartServer()
 }
